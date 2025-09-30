32d7c5ea75917c1b25ad04e771af497bae322ac7
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-30 10:43:42 +0500

    rotation

--- scripts/shared.gd
+++ scripts/shared.gd
@@ -1,13 +1,47 @@
 extends Node
 
-enum Tetromino { I, O, T, J, L, S, Z }
+enum TetrominoType { I, O, T, J, L, S, Z }
 
-var cells: Dictionary[Tetromino, Array] = {
-	Tetromino.I: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
-	Tetromino.O: [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
-	Tetromino.T: [Vector2(0, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.J: [Vector2(-1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.L: [Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.S: [Vector2(0, 1), Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0)],
-	Tetromino.Z: [Vector2(-1, 1), Vector2(0, 1), Vector2(0, 0), Vector2(1, 0)],
+var cells: Dictionary[TetrominoType, Array] = {
+	TetrominoType.I: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
+	TetrominoType.O: [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
+	TetrominoType.T: [Vector2(0, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+	TetrominoType.J: [Vector2(-1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+	TetrominoType.L: [Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+	TetrominoType.S: [Vector2(0, 1), Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0)],
+	TetrominoType.Z: [Vector2(-1, 1), Vector2(0, 1), Vector2(0, 0), Vector2(1, 0)],
 }
+var wall_kicks_i: Array = [
+	[Vector2(0,0), Vector2(-2,0), Vector2(1,0), Vector2(-2,-1), Vector2(1,2)],
+	[Vector2(0,0), Vector2(2,0), Vector2(-1, 0), Vector2(2,1), Vector2(-1, -2)],
+	[Vector2(0,0), Vector2(-1, 0), Vector2(2,0), Vector2(-1,2), Vector2(2, -1)],
+	[Vector2(0,0), Vector2(1,0), Vector2(-2, 0), Vector2(1, -2), Vector2(-2, 1)],
+	[Vector2(0,0), Vector2(2,0), Vector2(-1, 0), Vector2(2,1), Vector2(-1, -2)],
+	[Vector2(0,0), Vector2(-2,0), Vector2(1, 0), Vector2(-2, -1), Vector2(1, 2)],
+	[Vector2(0,0), Vector2(1,0), Vector2(-2,0), Vector2(1, -2), Vector2(-2,1)],
+	[Vector2(0,0), Vector2(-1, 0), Vector2(2, 0), Vector2(-1,2), Vector2(2, -1)]
+]
+
+var wall_kicks_jlostz: Array = [
+	[Vector2(0,0), Vector2(-1,0), Vector2(-1,1), Vector2(0,-2), Vector2(-1, -2)],
+	[Vector2(0,0), Vector2(1,0), Vector2(1, -1), Vector2(0,2), Vector2(1, 2)],
+	[Vector2(0,0), Vector2(1, 0), Vector2(1,-1), Vector2(0,2), Vector2(1, 2)],
+	[Vector2(0,0), Vector2(-1,0), Vector2(-1, 1), Vector2(0, -2), Vector2(-1, -2)],
+	[Vector2(0,0), Vector2(1,0), Vector2(1, 1), Vector2(0,-2), Vector2(1, -2)],
+	[Vector2(0,0), Vector2(-1,0), Vector2(-1, -1), Vector2(0, 2), Vector2(-1, 2)],
+	[Vector2(0,0), Vector2(-1,0), Vector2(-1,-1), Vector2(0, 2), Vector2(-1, 2)],
+	[Vector2(0,0), Vector2(1, 0), Vector2(1, 1), Vector2(0,-2), Vector2(1, -2)]
+]
+
+#var data = {
+	#TetrominoType.I: preload("res://Resources/i_piece_data.tres"),
+	#TetrominoType.J: preload("res://Resources/j_piece_data.tres"),
+	#TetrominoType.L: preload("res://Resources/l_piece_data.tres"),
+	#TetrominoType.O: preload("res://Resources/o_piece_data.tres"),
+	#TetrominoType.S: preload("res://Resources/s_piece_data.tres"),
+	#TetrominoType.T: preload("res://Resources/t_piece_data.tres"),
+	#TetrominoType.Z: preload("res://Resources/z_piece_data.tres")
+#}
+
+var clockwise_rotation_matrix: Array = [Vector2(0, -1), Vector2(1, 0)]
+var counter_clockwise_rotation_matrix: Array = [Vector2(0,1), Vector2(-1, 0)]
