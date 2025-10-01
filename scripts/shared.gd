6c0e7957d1dddce7bf8d30a6471288f66d9c1afe
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-10-01 10:41:45 +0500

    piexes

--- scripts/shared.gd
+++ scripts/shared.gd
@@ -33,15 +33,15 @@ var wall_kicks_jlostz: Array = [
 	[Vector2(0,0), Vector2(1, 0), Vector2(1, 1), Vector2(0,-2), Vector2(1, -2)]
 ]
 
-#var data = {
-	#TetrominoType.I: preload("res://Resources/i_piece_data.tres"),
-	#TetrominoType.J: preload("res://Resources/j_piece_data.tres"),
-	#TetrominoType.L: preload("res://Resources/l_piece_data.tres"),
-	#TetrominoType.O: preload("res://Resources/o_piece_data.tres"),
-	#TetrominoType.S: preload("res://Resources/s_piece_data.tres"),
-	#TetrominoType.T: preload("res://Resources/t_piece_data.tres"),
-	#TetrominoType.Z: preload("res://Resources/z_piece_data.tres")
-#}
+var data = {
+	TetrominoType.I: preload("res://Resources/i_piece_data.tres"),
+	TetrominoType.J: preload("res://Resources/j_piece_data.tres"),
+	TetrominoType.L: preload("res://Resources/l_piece_data.tres"),
+	TetrominoType.O: preload("res://Resources/o_piece_data.tres"),
+	TetrominoType.S: preload("res://Resources/s_piece_data.tres"),
+	TetrominoType.T: preload("res://Resources/t_piece_data.tres"),
+	TetrominoType.Z: preload("res://Resources/z_piece_data.tres")
+}
 
 var clockwise_rotation_matrix: Array = [Vector2(0, -1), Vector2(1, 0)]
 var counter_clockwise_rotation_matrix: Array = [Vector2(0,1), Vector2(-1, 0)]
